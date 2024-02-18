870d81261881ef5e0bbb003b2c8eee98a2c9b43a
liyi <lrwlisense@163.com>
2024-02-18 15:38:00 +0800

    optimize http check

--- app/httpCheck.go
+++ app/httpCheck.go
@@ -2,13 +2,12 @@ package app
 
 import (
 	"errors"
-	"fmt"
-	"github.com/gin-gonic/gin"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"net/url"
 	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 type requestHttpCheck struct {
@@ -39,11 +38,13 @@ func HttpCheck(c *gin.Context) {
 	var res responseHttpCheck
 	// 获取参数
 	param := requestHttpCheck{}
-	err := c.BindJSON(&param)
-	if err != nil {
+	if err := c.BindJSON(&param); err != nil {
 		res.ErrMsg = "解析参数失败"
 		res.Err = err.Error()
-		c.JSON(500, res)
+		c.AbortWithError(http.StatusInternalServerError, &gin.Error{
+			Err:  err,
+			Type: gin.ErrorTypePublic,
+		})
 		return
 	}
 
@@ -52,29 +53,32 @@ func HttpCheck(c *gin.Context) {
 	}
 
 	httpRes, err := client.Get(param.Url)
-
 	if err != nil {
-		fmt.Printf("%#v\n", err)
-
-		var urlErr *url.Error
-		if errors.As(err, &urlErr) {
-			res.CheckResCode = 1 // 约定如果检查异常， 返回码为1
-			res.CheckRes = urlErr.Error()
-		}
-		c.JSON(200, res)
+		handleError(c, err, res)
 		return
 	}
 
 	body, err := ioutil.ReadAll(httpRes.Body)
 	if err != nil {
-		log.Printf("[ERROR][PostCheck][read res body] %s\n", err.Error())
-		res.CheckResCode = 1 // 约定读取body异常， 返回码为1
-		res.CheckRes = err.Error()
-		c.JSON(200, res)
+		handleError(c, err, res)
 		return
 	}
+	httpRes.Body.Close() // 确保关闭响应体
+
 	res.CheckRes = httpRes.Status
 	res.CheckResCode = httpRes.StatusCode
 	res.Res = string(body)
-	c.JSON(200, res)
+	c.JSON(http.StatusOK, res)
+}
+
+func handleError(c *gin.Context, err error, res responseHttpCheck) {
+	var urlErr *url.Error
+	if errors.As(err, &urlErr) {
+		res.CheckResCode = 1 // 约定如果检查异常， 返回码为1
+		res.CheckRes = urlErr.Error()
+	} else {
+		res.ErrMsg = "请求失败"
+		res.Err = err.Error()
+	}
+	c.AbortWithError(http.StatusInternalServerError, err)
 }
