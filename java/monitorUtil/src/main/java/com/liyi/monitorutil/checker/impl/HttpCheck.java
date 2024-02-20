aa60c699cf04f2e968b4af3d5481d5d2e286bda4
liyi <lrwlisense@163.com>
2024-02-20 18:13:28 +0800

    add log

--- java/monitorUtil/src/main/java/com/liyi/monitorutil/checker/impl/HttpCheck.java
+++ java/monitorUtil/src/main/java/com/liyi/monitorutil/checker/impl/HttpCheck.java
@@ -4,11 +4,16 @@ import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpResponse;
 import cn.hutool.http.HttpStatus;
 import com.liyi.monitorutil.checker.Checker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HttpCheck implements Checker {
+
+    private static final Logger LOGGER  = LoggerFactory.getLogger(HttpCheck.class);
     public boolean check(String url){
+        LOGGER.info("url:{}",url);
         try(HttpResponse response = HttpRequest.get(url).timeout(10*1000).execute()) {
             if (response.getStatus()>= HttpStatus.HTTP_BAD_REQUEST){
                 return false;
@@ -16,6 +21,7 @@ public class HttpCheck implements Checker {
                 return true;
             }
         } catch (Exception e){
+            LOGGER.error("httpCheck error.",e);
             return false;
         }
     }
